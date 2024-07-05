1d9d6a130c8db2259409776af419a81abc3b9594
phamquyetthang <quyetthang.phucchi@gmail.com>
2024-07-05 15:40:56 +0700

    DeepFlattenedTypePaths

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { DotNestedKeys } from "./utils/dotNestedKey";
 
 import { ChildItemType, ChildType } from "./utils/childType";
+import {
+  DeepFlattenedTypePaths,
+  FlattenedTypePaths,
+} from "./utils/FlattenedTypePaths";
 
 export {
   DotNestedKeys,
   ChildItemType,
-  ChildType
-}
+  ChildType,
+  FlattenedTypePaths,
+  DeepFlattenedTypePaths,
+};
